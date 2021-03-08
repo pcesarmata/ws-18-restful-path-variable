4c1d00057809692266a3aa34cffad6de021dc912
Cesa <pcesarmata@gmail.com>
2021-03-08 12:00:44 -0600

    Init commit

---
+++ src/main/java/com/cesarmata/Ws18RestfulPathVariableApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws18RestfulPathVariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws18RestfulPathVariableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/Ws18RestfulPathVariableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws18RestfulPathVariableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
